2def043f871f7d8745efae8c3acdbaa75416d3c4
vincentlee1218 <44049108+vincentlee1218@users.noreply.github.com>
2020-03-03 15:07:17 -0800

    A4 - IntArrayWorker
    creating methods for getCount, getLargest, and getColTotal

--- pixlab/IntArrayWorker.java
+++ pixlab/IntArrayWorker.java
@@ -45,6 +45,50 @@ public class IntArrayWorker
     return total;
   }
   
+    /**
+     * Method to get the count of the number of times a passed integer value
+     * is found in the matrix.
+     * @param number the number to look for in the matrix
+     * @return the number of times the passed integer value is found in matrix
+     */
+    public int getCount(int number) {
+        int count=0;
+        for (int[] rowArray : matrix) {
+            for (int item : rowArray) {
+                if (item == number) count++;
+            }
+        }
+        return count;
+    }
+  
+    /**
+     * Method to get the largest integer value in the matrix
+     * @return the largest integer value in the matrix
+     */
+    public int getLargest() {
+        int max = matrix[0][0];
+        for (int[] rowArray : matrix) {
+            for (int item : rowArray) {
+                if (item > max) max = item;
+            }
+        }
+        return max;
+    }
+    
+    /**
+     * Method to return the total of a specified column.
+     * <br> Assumes that the matrix is rectangular (not jagged)
+     * @param col the column to total up
+     * @return total of all integers in the column
+     */
+    public int getColTotal(int col) {
+        int total=0;
+        for (int i = 0; i < matrix.length; i++) {
+            total += matrix[i][col];
+        }
+        return total;
+    }
+    
   /**
    * Method to fill with an increasing count
    */
